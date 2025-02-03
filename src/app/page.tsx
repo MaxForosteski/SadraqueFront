2833f98998aa3ba658119068f1aa15ee9341a0ee
Max <maxforosteski12@gmail.com>
2025-02-03 17:07:24 -0300

    estrutura do navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "../../public/components/navbar";
 
 export default function Home() {
   return (
     <div className="main">
-      
+      <Navbar />
     </div>
   );
 }
