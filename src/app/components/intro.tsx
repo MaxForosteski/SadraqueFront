345eadf464f44570bc1b1a46912104ea3974f8d8
Marcos Max Forosteski da Silva <max.silva@adami.com.br>
2025-02-11 08:30:59 -0300

    terminado base intro

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -2,19 +2,19 @@ import Image from "next/image"
 
 export default function Intro() {
     return (
-        <div className="flex">
-            <div className="w-1/2 mt-10 ml-10 mr-5 text-intro">
-                <h2 className="mb-10"><span className="text-highlight">Sadraque Auto Center</span> – Cuidado e Qualidade para Seu Carro!</h2>
-                <p >Manutenção, reparos e peças sob demanda com <span className="text-highlight">atendimento de confiança</span>. Tudo para manter <span className="text-highlight">seu carro seguro</span> e em <span className="text-highlight">perfeito estado</span>.</p>
+        <div className="flex intro">
+            <div className="w-2/4 mt-10 text-intro">
+                <h1 className="mb-10 text-4xl"><span className="text-highlight">Sadraque Auto Center</span> – Cuidado e Qualidade para Seu Carro!</h1>
+                <p className="text-2xl">Manutenção, reparos e peças sob demanda com <span className="text-highlight">atendimento de confiança</span>. Tudo para manter <span className="text-highlight">seu carro seguro</span> e em <span className="text-highlight">perfeito estado</span>.</p>
             </div>
 
-            <div className="w-1/2 mt-10 carro-intro">
+            <div className="carro-intro">
                 <Image
                     src={"/images/carro-intro.jpg"}
                     alt="Carro"
-                    width={450}
-                    height={450}
-                    style={{ borderRadius: "10%" }}
+                    width={550}
+                    height={550}
+                    style={{borderRadius: "10px", width:"100%",height:"100%"}}
                 />
             </div>
         </div>
