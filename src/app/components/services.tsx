6318b5422bd0d6f199150e4418062b124b944621
Marcos Max Forosteski da Silva <max.silva@adami.com.br>
2025-02-12 10:21:29 -0300

    Criado servicos,hotfix navbar e criado referencias

---
+++ src/app/components/services.tsx
@@ -0,0 +1,17 @@
+
+export default function Servicos(){
+
+    return(
+        <div className="servicos">
+            <div className="maintenance">
+
+            </div>
+            <div className="security">
+
+            </div>
+            <div className="revision">
+                
+            </div>
+        </div>
+    )
+}
