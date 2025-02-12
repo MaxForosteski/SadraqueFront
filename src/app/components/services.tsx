b6f702676f7ff3affe00215ca895268a950bd0c2
Marcos Max Forosteski da Silva <max.silva@adami.com.br>
2025-02-12 16:01:18 -0300

    Criado base de apresentação de serviços

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -1,17 +1,33 @@
+import Image from "next/image"
 
-export default function Servicos(){
+export default function Services() {
 
-    return(
-        <div className="servicos">
-            <div className="maintenance">
+    return (
+        <div className="services text-black text-4xl">
+            <h1 className="pl-10 mt-10">Serviços</h1>
 
+            <div className="flex justify-center">
+                <div className="w-10/12 p-5 flex justify-between bg-gray mt-10 rounded-3xl">
+                    <div className="maintenance flex flex-col items-center text-center text-xl m-10">
+                        <Image src={"/images/maintenance.png"} alt="Manutenção" width={100} height={100} />
+                        <h3 className="font-bold mt-5">Manutenção Completa</h3>
+                        <p className="mt-5">Motor, câmbio e injeção eletrônica.</p>
+                    </div>
+                    <div className="security flex flex-col items-center text-center text-xl m-10">
+                        <Image src={"/images/escudo.png"} alt="Escudo" width={100} height={100} />
+                        <h3 className="font-bold mt-5">Segurança e Conforto</h3>
+                        <p className="mt-5">Freios, suspensão e direção.</p>
+                    </div>
+                    <div className="revision flex flex-col items-center text-center text-xl m-10">
+                        <Image src={"/images/chave-inglesa.png"} alt="chave inglesa" width={100} height={100} />
+                        <h3 className="font-bold mt-5">Troca de Óleo e Revisões</h3>
+                        <p className="mt-5">Mais desempenho e durabilidade.</p>
+                    </div>
+                </div>
             </div>
-            <div className="security">
 
-            </div>
-            <div className="revision">
-                
-            </div>
+
+
         </div>
     )
 }
