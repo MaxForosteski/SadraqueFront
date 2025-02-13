649d9e9b1750805db1b1fc95a7bbe30c5b222357
Max <maxforosteski12@gmail.com>
2025-02-12 23:24:12 -0300

    adicionado animação de surgimento

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -1,12 +1,25 @@
+"use client"
+
 import Image from "next/image"
+import { useEffect } from "react"
 
 export default function Services() {
-
+    useEffect(() => {
+        const services : any = document.getElementById("services")
+        const observer = new IntersectionObserver((entries:any) => {
+            entries.forEach((entry: { isIntersecting: any; target: { style: { opacity: number; transform: string; transition: string } } }) => {
+                if (entry.isIntersecting) {
+                    services.classList.add("visivel");
+                }
+            })
+        }, { threshold: 0.1 })
+        observer.observe(services)
+    }, [])
     return (
         <div className="services text-black text-4xl">
             <h1 className="pl-10 mt-10">Serviços</h1>
 
-            <div className="flex justify-center">
+            <div className="flex justify-center " id="services">
                 <div className="w-10/12 p-5 flex justify-between bg-gray mt-10 rounded-3xl">
                     <div className="maintenance flex flex-col items-center text-center text-xl m-10">
                         <Image src={"/images/maintenance.png"} alt="Manutenção" width={100} height={100} />
