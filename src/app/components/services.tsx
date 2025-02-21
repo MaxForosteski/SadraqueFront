6c6bb1620caab3453517cad1ff6fbf7a375ca71b
Max <maxforosteski12@gmail.com>
2025-02-21 05:49:24 -0300

    alterado layout para estilo group button

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -22,19 +22,19 @@ export default function Services() {
         <div id="servicos" className="services text-black text-4xl">
             <h1 className="pl-10 mt-10">Serviços</h1>
 
-            <div className="flex justify-center " id="services">
-                <div className="w-10/12 p-5 flex justify-between bg-gray mt-10 rounded-3xl">
-                    <div className="maintenance flex flex-col items-center text-center text-xl m-10">
+            <div className="flex justify-center" id="services">
+                <div className="w-10/12 p-5 flex justify-center mt-10 rounded-3xl">
+                    <div className="maintenance flex p-10 rounded-s-3xl bg-gray flex-col items-center text-center text-xl">
                         <Image src={"/images/maintenance.png"} alt="Manutenção" width={100} height={100} />
                         <h3 className="font-bold mt-5">Manutenção Completa</h3>
                         <p className="mt-5">Motor, câmbio e injeção eletrônica.</p>
                     </div>
-                    <div className="security flex flex-col items-center text-center text-xl m-10">
+                    <div className="security flex p-10 bg-gray flex-col bg-gray items-center text-center text-xl">
                         <Image src={"/images/escudo.png"} alt="Escudo" width={100} height={100} />
                         <h3 className="font-bold mt-5">Segurança e Conforto</h3>
                         <p className="mt-5">Freios, suspensão e direção.</p>
                     </div>
-                    <div className="revision flex flex-col items-center text-center text-xl m-10">
+                    <div className="revision flex p-10 rounded-e-3xl bg-gray flex-col bg-gray items-center text-center text-xl">
                         <Image src={"/images/chave-inglesa.png"} alt="chave inglesa" width={100} height={100} />
                         <h3 className="font-bold mt-5">Troca de Óleo e Revisões</h3>
                         <p className="mt-5">Mais desempenho e durabilidade.</p>
