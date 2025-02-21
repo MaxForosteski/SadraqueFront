47190fdf301342885bc73de001f26d3e4cb1128a
Max <maxforosteski12@gmail.com>
2025-02-21 06:11:04 -0300

    iniciado construção popup manutenção

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react"
 
 export default function Services() {
     useEffect(() => {
+
         const services: HTMLDivElement | null = document.getElementById("services") as HTMLDivElement | null
         const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
             entries.forEach((entry: { isIntersecting: boolean }) => {
@@ -24,21 +25,30 @@ export default function Services() {
 
             <div className="flex justify-center" id="services">
                 <div className="w-10/12 p-5 flex justify-center mt-10 rounded-3xl">
-                    <div className="maintenance flex p-10 rounded-s-3xl bg-gray flex-col items-center text-center text-xl">
-                        <Image src={"/images/maintenance.png"} alt="Manutenção" width={100} height={100} />
-                        <h3 className="font-bold mt-5">Manutenção Completa</h3>
-                        <p className="mt-5">Motor, câmbio e injeção eletrônica.</p>
+                    <div className="maintenance-group">
+                        <div className="maintenance flex p-10 rounded-s-3xl bg-gray flex-col items-center text-center text-xl">
+                            <Image src={"/images/maintenance.png"} alt="Manutenção" width={100} height={100} />
+                            <h3 className="font-bold mt-5">Manutenção Completa</h3>
+                            <p className="mt-5">Motor, câmbio e injeção eletrônica.</p>
+                        </div>
+                        <div className="maintenance-popup">
+                            <p>testesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+                        </div>
                     </div>
+
+
                     <div className="security flex p-10 bg-gray flex-col bg-gray items-center text-center text-xl">
                         <Image src={"/images/escudo.png"} alt="Escudo" width={100} height={100} />
                         <h3 className="font-bold mt-5">Segurança e Conforto</h3>
                         <p className="mt-5">Freios, suspensão e direção.</p>
                     </div>
+
                     <div className="revision flex p-10 rounded-e-3xl bg-gray flex-col bg-gray items-center text-center text-xl">
                         <Image src={"/images/chave-inglesa.png"} alt="chave inglesa" width={100} height={100} />
                         <h3 className="font-bold mt-5">Troca de Óleo e Revisões</h3>
                         <p className="mt-5">Mais desempenho e durabilidade.</p>
                     </div>
+
                 </div>
             </div>
 
