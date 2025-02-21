50dedc31a710b1bd1861a53f9c9ba8348e4feb74
Marcos Max Forosteski da Silva <maxforosteski12@gmail.com>
2025-02-21 16:55:04 -0300

    continuacao popup servicos

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 import Image from "next/image"
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 
 export default function Services() {
     useEffect(() => {
-
         const services: HTMLDivElement | null = document.getElementById("services") as HTMLDivElement | null
         const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
             entries.forEach((entry: { isIntersecting: boolean }) => {
@@ -19,6 +18,19 @@ export default function Services() {
         }
 
     }, [])
+    const maintenance_popup: HTMLDivElement | null = document.getElementById("maintenance-popup") as HTMLDivElement | null;
+
+    function OnMouseMaintenance(event: React.MouseEvent<HTMLDivElement>) {
+        if (maintenance_popup) {
+            maintenance_popup.classList.add("visible")
+        }
+    }
+    const OutMouseMaintenance = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (maintenance_popup) {
+            maintenance_popup.classList.remove("visible")
+        }
+    }
+
     return (
         <div id="servicos" className="services text-black text-4xl">
             <h1 className="pl-10 mt-10">Serviços</h1>
@@ -26,12 +38,12 @@ export default function Services() {
             <div className="flex justify-center" id="services">
                 <div className="w-10/12 p-5 flex justify-center mt-10 rounded-3xl">
                     <div className="maintenance-group">
-                        <div className="maintenance flex p-10 rounded-s-3xl bg-gray flex-col items-center text-center text-xl">
+                        <div id="maintenance" onMouseEnter={OnMouseMaintenance} onMouseLeave={OutMouseMaintenance} className="maintenance flex p-10 rounded-s-3xl bg-gray flex-col items-center text-center text-xl">
                             <Image src={"/images/maintenance.png"} alt="Manutenção" width={100} height={100} />
                             <h3 className="font-bold mt-5">Manutenção Completa</h3>
                             <p className="mt-5">Motor, câmbio e injeção eletrônica.</p>
                         </div>
-                        <div className="maintenance-popup">
+                        <div id="maintenance-popup" className="maintenance-popup">
                             <p>testesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
                         </div>
                     </div>
