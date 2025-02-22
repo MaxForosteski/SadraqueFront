ef5d9f76ddd524e8dc45cf476d7c34df676afe9e
Max <maxforosteski12@gmail.com>
2025-02-21 23:06:54 -0300

    criado popups dos servicos

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -1,9 +1,25 @@
 "use client"
 
 import Image from "next/image"
-import React, { useEffect } from "react"
+import React, { useCallback, useEffect } from "react"
 
 export default function Services() {
+
+    /*const maintenance = document.getElementById("maintenance") as HTMLDivElement | null;
+    const maintenance_popup = document.getElementById("maintenance-popup") as HTMLDivElement | null;
+
+    const OnMouseMaintenance = useCallback((event: MouseEvent) => {
+        if (maintenance_popup) {
+            maintenance_popup.classList.add("visible")
+        }
+    },[])
+
+    const OutMouseMaintenance = useCallback((event: MouseEvent) => {
+        if (maintenance_popup) {
+            maintenance_popup.classList.remove("visible")
+        }
+    },[])*/
+
     useEffect(() => {
         const services: HTMLDivElement | null = document.getElementById("services") as HTMLDivElement | null
         const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
@@ -17,19 +33,21 @@ export default function Services() {
             observer.observe(services)
         }
 
-    }, [])
-    const maintenance_popup: HTMLDivElement | null = document.getElementById("maintenance-popup") as HTMLDivElement | null;
 
-    function OnMouseMaintenance(event: React.MouseEvent<HTMLDivElement>) {
-        if (maintenance_popup) {
-            maintenance_popup.classList.add("visible")
+        /*
+        if(maintenance){
+            maintenance.addEventListener("mouseleave", OutMouseMaintenance);
+            maintenance.addEventListener("mouseenter", OnMouseMaintenance);
         }
-    }
-    const OutMouseMaintenance = (event: React.MouseEvent<HTMLDivElement>) => {
-        if (maintenance_popup) {
-            maintenance_popup.classList.remove("visible")
+
+        return () => {
+            if(maintenance){
+                maintenance.removeEventListener("mouseenter", OnMouseMaintenance);
+                maintenance.removeEventListener("mouseleave", OutMouseMaintenance);
+            }
         }
-    }
+        */
+    }, []);
 
     return (
         <div id="servicos" className="services text-black text-4xl">
@@ -38,34 +56,80 @@ export default function Services() {
             <div className="flex justify-center" id="services">
                 <div className="w-10/12 p-5 flex justify-center mt-10 rounded-3xl">
                     <div className="maintenance-group">
-                        <div id="maintenance" onMouseEnter={OnMouseMaintenance} onMouseLeave={OutMouseMaintenance} className="maintenance flex p-10 rounded-s-3xl bg-gray flex-col items-center text-center text-xl">
+                        <div
+                            id="maintenance"
+                            onMouseEnter={() => { document.getElementById("maintenance-popup")?.classList.add("visible"); }}
+                            onMouseLeave={() => { document.getElementById("maintenance-popup")?.classList.remove("visible"); }}
+                            className="maintenance flex p-10 rounded-s-3xl bg-gray flex-col items-center text-center text-xl"
+                        >
+
                             <Image src={"/images/maintenance.png"} alt="Manutenção" width={100} height={100} />
                             <h3 className="font-bold mt-5">Manutenção Completa</h3>
                             <p className="mt-5">Motor, câmbio e injeção eletrônica.</p>
                         </div>
-                        <div id="maintenance-popup" className="maintenance-popup">
-                            <p>testesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+                        <div id="maintenance-popup" className="maintenance-popup text-lg">
+                            <p>Manutenção Geral 🚗🔧</p>
+                            <p>Inclui:</p>
+                            <p>✅ Revisão e manutenção do motor</p>
+                            <p>✅ Verificação e ajuste da caixa de câmbio</p>
+                            <p>✅ Limpeza completa: bicos injetores, radiador e linha de combustível</p>
+                            <p>✅ Troca de mangueiras (água e combustível)</p>
+                            <p>✅ Substituição da bateria</p>
+                            <p>✅ Troca das correias do motor</p>
+                            <p>E muito mais!</p>
                         </div>
                     </div>
 
+                    <div className="security-group">
+                        <div
+                            className="security flex p-10 bg-gray flex-col bg-gray items-center text-center text-xl"
+                            onMouseEnter={() => { document.getElementById("security-popup")?.classList.add("visible"); }}
+                            onMouseLeave={() => { document.getElementById("security-popup")?.classList.remove("visible"); }}
+                        >
+                            <Image src={"/images/escudo.png"} alt="Escudo" width={100} height={100} />
+                            <h3 className="font-bold mt-5">Segurança e Conforto</h3>
+                            <p className="mt-5">Freios, suspensão e direção.</p>
+                        </div>
+                        <div id="security-popup" className="security-popup text-lg">
+                            <p>🔧 Segurança e Conforto para o Seu Veículo 🚗💨</p>
 
-                    <div className="security flex p-10 bg-gray flex-col bg-gray items-center text-center text-xl">
-                        <Image src={"/images/escudo.png"} alt="Escudo" width={100} height={100} />
-                        <h3 className="font-bold mt-5">Segurança e Conforto</h3>
-                        <p className="mt-5">Freios, suspensão e direção.</p>
+                            <p>✔ Revisão completa das linhas hidráulicas de freio</p>
+                            <p>✔ Inspeção e troca de lonas, pastilhas e discos de freio</p>
+                            <p>✔ Avaliação da suspensão: pivôs, terminais, coxins, bandejas e buchas</p>
+                            <p>✔ Verificação da direção: elétrica, hidráulica e mecânica</p>
+                            <p>✔ Desobstrução dos dutos de ar para melhor ventilação</p>
+                            <p>✔ E muito mais!</p>
+
+                            <p>📅 Agende já a sua revisão e dirija com segurança!</p>
+                        </div>
                     </div>
 
-                    <div className="revision flex p-10 rounded-e-3xl bg-gray flex-col bg-gray items-center text-center text-xl">
-                        <Image src={"/images/chave-inglesa.png"} alt="chave inglesa" width={100} height={100} />
-                        <h3 className="font-bold mt-5">Troca de Óleo e Revisões</h3>
-                        <p className="mt-5">Mais desempenho e durabilidade.</p>
-                    </div>
+                    <div className="revision-group">
+                        <div className="revision flex p-10 rounded-e-3xl bg-gray flex-col bg-gray items-center text-center text-xl"
+                            onMouseEnter={() => { document.getElementById("revision-popup")?.classList.add("visible"); }}
+                            onMouseLeave={() => { document.getElementById("revision-popup")?.classList.remove("visible"); }}
+                        >
+                            <Image src={"/images/chave-inglesa.png"} alt="chave inglesa" width={100} height={100} />
+                            <h3 className="font-bold mt-5">Troca de Óleo e Revisões</h3>
+                            <p className="mt-5">Mais desempenho e durabilidade.</p>
+                        </div>
 
+                        <div className="revision-popup text-lg" id="revision-popup">
+                            <p>🔧 Troca de Óleo, Fluidos e Revisões Completa! 🚗💨</p>
+
+                            <p>✅ Troca de óleo: motor, câmbio e direção</p>
+                            <p>✅ Substituição dos fluidos: freio e arrefecimento (radiador)</p>
+                            <p>✅ Troca de filtros: óleo, combustível, ar do motor e ar-condicionado</p>
+                            <p>✅ Revisão completa de velas e cabos de vela</p>
+                            <p>✅ E muito mais!</p>
+                        </div>
+
+                    </div>
                 </div>
+
+
+
             </div>
-
-
-
         </div>
     )
 }
