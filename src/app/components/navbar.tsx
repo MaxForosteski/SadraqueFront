e0d3c9b457efe3a214408d146e451eca383d8c55
Marcos Max Forosteski da Silva <maxforosteski12@gmail.com>
2025-02-28 16:51:16 -0300

    iniciado adaptação do site para modelos mobile e notebooks

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,20 +1,27 @@
+
 import Image from "next/image"
 
-export default function Navbar() {
+
+export default function Navbar({ isMobile }: { isMobile: boolean }) {
+
     return (
         <div className="navbar text-xl">
             <ul>
-                    <Image
-                        src={"/images/Sadraque-logo.jpg"}
-                        alt="Sadraque Auto center"
-                        width={1000}
-                        height={1000}
-                        style={{width: "200px", height: "90px"}}
-                    />
-                <li><a href="#home">Home</a></li>
-                <li><a href="#">Loja</a></li>
-                <li><a href="#servicos">Serviços</a></li>
-                <li><a href="#sobrenos">Sobre nós</a></li>
+                <Image
+                    src={"/images/Sadraque-logo.jpg"}
+                    alt="Sadraque Auto center"
+                    width={1000}
+                    height={1000}
+                    style={{ width: "200px", height: "90px" }}
+                />
+                {isMobile ? <></> :
+                    <>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#">Loja</a></li>
+                        <li><a href="#servicos">Serviços</a></li>
+                        <li><a href="#sobrenos">Sobre nós</a></li>
+                    </>
+                }
             </ul>
         </div>
     )
